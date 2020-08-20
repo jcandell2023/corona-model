bde95473633a7a1736a551d4d526becdb4c09680
Jeff Candell <jcandell@wustl.edu>
2020-08-20 11:14:21 -0400

    Change to line plot for all

--- src/App.svelte
+++ src/App.svelte
@@ -32,14 +32,6 @@
     limits.push(data[i][2][0])
   }
 
-  let limitData = []
-
-  $: {
-    for (let i = 0; i < data.length; i++) {
-      limitData[i] = limits[i] - lineData[day - 1][i]
-    }
-  }
-
   let interval
 
   const playAnimation = () => {
@@ -69,7 +61,11 @@
 <div class="container">
   <h1 class="display-4 text-center">Corona Virus Model</h1>
   <h3 class="text-center">{date.toLocaleDateString()}</h3>
-  <Plot circleData={dotData[day - 1]} lineData={lineData[day - 1]} {labels} {limitData} />
+  <Plot
+    circleData={dotData[day - 1]}
+    lineData={lineData[day - 1]}
+    {labels}
+    limitData={limits} />
   <div class="row">
     <p class="col-lg-1">{date.toLocaleDateString()}</p>
     <input
